5c099b04ddb873a7e3521464fff28f32e65eecfd
jossanchezal <jossanchezal@unal.edu.co>
2024-11-12 11:36:44 -0500

    arreglo4

--- src/main/java/Motor.java
+++ src/main/java/Motor.java
@@ -1,22 +1,23 @@
+package test;
+
 public class Motor {
     int numeroCilindros;
     String tipo;
     int registro;
     
-    public Motor(int numeroCilindros, String tipo, int registro) {
-        this.numeroCilindros = numeroCilindros;
-        this.tipo = tipo;
-        this.registro= registro;
-    }
-
     public void cambiarRegistro(int nuevoRegistro){
         this.registro = nuevoRegistro;
     }
 
 
     public void asignarTipo(String nuevoTipo){
-        if (nuevoTipo.equals("electrico") || nuevoTipo.equals("gasolina")){
-            this.tipo = nuevoTipo;
+        switch (nuevoTipo) {
+            case "gasolina": this.tipo=nuevoTipo;
+                break;
+            case "electrico": this.tipo=nuevoTipo;
+                break;
+            default:
+                break;;
         }
     }
 }
